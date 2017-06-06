0d271872a358bfdc8f0d9fc5eca295c70c3417d9
clousqt <clous@live.com>
2017-06-06 10:15:38 -0700

    Update problem_3.py

--- Week 1/problem_set_1/problem_3.py
+++ Week 1/problem_set_1/problem_3.py
@@ -1,12 +1,12 @@
-#   Assume s is a string of lower case characters.
+# Assume s is a string of lower case characters.
 #
-#   Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print
+# Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print
 #
-#   Longest substring in alphabetical order is: beggh
+# Longest substring in alphabetical order is: beggh
 #
-#   In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
+# In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print
 #
-#   Longest substring in alphabetical order is: abc
+# Longest substring in alphabetical order is: abc
 
 
 streak = ''
