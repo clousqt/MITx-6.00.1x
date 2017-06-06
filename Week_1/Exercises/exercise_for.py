d95c243b529618d522539d2e3f8c3d1762d302ea
clousqt <clous@live.com>
2017-06-06 10:42:22 -0700

    Create exercise_for.py

---
+++ Week_1/Exercises/exercise_for.py
@@ -0,0 +1,13 @@
+# Convert the following code into code that uses a for loop.
+#
+# prints 2
+# prints 4
+# prints 6
+# prints 8
+# prints 10
+# prints "Goodbye!"
+
+for num in range(0,10,2):
+    num+=2
+    print(num)
+print("Goodbye!")
