2ca1fddcfea0bbb0d6208f4ac48f6019f7130ab8
clousqt <clous@live.com>
2017-06-06 10:41:03 -0700

    Create exercise_while.py

---
+++ Week_1/Exercises/exercise_while.py
@@ -0,0 +1,14 @@
+# Convert the following into code that uses a while loop.
+#
+# print 2
+# prints 4
+# prints 6
+# prints 8
+# prints 10
+# prints Goodbye!
+
+num=0
+while num < 10:
+    num+=2
+    print(num)   
+print("Goodbye!")
